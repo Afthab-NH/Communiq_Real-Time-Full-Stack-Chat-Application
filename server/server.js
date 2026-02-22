56441136289bd50ca261f18b0c10f3af98cf19d1
Afthab NH <afthabmohammed45@gmail.com>
2026-02-22 13:22:53 +0530

    database connection establishment

--- server/server.js
+++ server/server.js
@@ -10,4 +10,10 @@ const server = http.createServer(app)
 
 //Middleware Setup
 
-//2:15:12
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+
+app.use("/api/status", (req, res) => res.send("Server is Live"));
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
