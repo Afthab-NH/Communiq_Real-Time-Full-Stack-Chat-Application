70461b2e2fffe1cbf37bed9d6311aa9cb3ac833e
Afthab NH <afthabmohammed45@gmail.com>
2026-02-22 16:13:43 +0530

    cloudinary setup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/User";
 import bcrypt from bcryptjs;
 import { generateToken } from "../lib/utils";
+import cloudinary from "../lib/cloudinary.js";
+
 
 
 // Sign Up new User
@@ -59,3 +61,32 @@ export const login = async (req, res) => {
     }
 }
 
+// Controller to check if user is authenticated
+
+export const checkAuth = (req, res) => {
+    res.json({ success: true, user: req.user });
+}
+
+// Contreoller to update user profile details
+
+export const updateProfile = async (req, res) => {
+    try {
+
+        const { profilePic, bio, fullName } = req.body;
+
+        const userId = req.user._id;
+        let updatedUser;
+
+        if(!profilePic){
+           updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName}, {new: true});
+        } else {
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName},{ new: true });
+        }
+        res.json({ success: true, user: updatedUser });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, user: error.message });
+    }
+}
