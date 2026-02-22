52615acb39c09fec8efe8694b3ef51b4d57e919c
Afthab NH <afthabmohammed45@gmail.com>
2026-02-22 16:19:30 +0530

    Route Establishment

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
-import { connect } from "http2";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 // Creating express app and HTTP server
 
@@ -15,7 +15,11 @@ const server = http.createServer(app)
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
+//Route Setup 
+
 app.use("/api/status", (req, res) => res.send("Server is Live"));
+app.use("/api/auth", userRouter);
+
 
 //Connect to MongoDB
 
