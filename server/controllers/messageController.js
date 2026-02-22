e2296c42d9278466b39f92bbaee9ba8ba930266d
Afthab NH <afthabmohammed45@gmail.com>
2026-02-22 20:50:26 +0530

    redirection established for users

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,8 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
+
 
 // Get all users except the logged in user
 
@@ -85,6 +87,13 @@ export const sendMessage = async (req, res) => {
             text,
             image: imageUrl
         })
+
+        // Emit the new message to the receiver's socket
+
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
 
         res.json({ success: true, newMessage });
 
