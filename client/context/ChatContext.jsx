3f1df134c59da9be5c4d5aa36a28825c508934af
Afthab NH <afthabmohammed45@gmail.com>
2026-03-07 11:48:22 +0530

    chat container updation

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -86,7 +86,7 @@ export const ChatProvider = ({ children }) => {
 
 
     const value = {
-        
+        messages, users, selectedUser, getUsers, setMessages, sendMessage, setSelectedUser, unseenMessages, setUnseenMessages
     }
 
     return (
